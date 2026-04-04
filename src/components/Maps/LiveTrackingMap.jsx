f1b34e61df236620bf4a0316dd2e9952fcf696fd
Nirbhay Khedekar <160317380+Nirbhaypatil24@users.noreply.github.com>
2026-04-04 22:16:20 +0530

    Update LiveTrackingMap.jsx

--- src/components/Maps/LiveTrackingMap.jsx
+++ src/components/Maps/LiveTrackingMap.jsx
@@ -104,7 +104,7 @@ const LiveTrackingMap = ({
   }, []);
 
   const connectStomp = () => {
-    const wsUrl = 'https://ec2-13-220-53-209.compute-1.amazonaws.com/ws-browser';
+    const wsUrl = 'https://ec2-3-109-49-80.ap-south-1.compute.amazonaws.com/ws-browser';
 
     const client = new Client({
       webSocketFactory: () => new WebSocket(wsUrl),
