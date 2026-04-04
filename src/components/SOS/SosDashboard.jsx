e76ab1fbb4cd81503716f8ef2ddfdea94ba360b5
Nirbhay Khedekar <160317380+Nirbhaypatil24@users.noreply.github.com>
2026-04-04 20:23:22 +0530

    Update SosDashboard.jsx

--- src/components/SOS/SosDashboard.jsx
+++ src/components/SOS/SosDashboard.jsx
@@ -50,7 +50,7 @@ const SosDashboard = () => {
   };
 
   const connectStomp = () => {
-    const wsUrl = 'wss://ec2-13-220-53-209.compute-1.amazonaws.com/ws-ricky';
+    const wsUrl = 'https://ec2-13-220-53-209.compute-1.amazonaws.com/ws-browser';
 
     const client = new Client({
       webSocketFactory: () => new WebSocket(wsUrl),
